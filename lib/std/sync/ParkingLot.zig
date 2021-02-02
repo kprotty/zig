e998eb60f2b170cef710bfb33902e851c0cc2eef
kprotty <kbutcher6200@gmail.com>
2021-02-01 21:44:36 -0600

    working on ParkingLot.DefaultLock

--- lib/std/sync/ParkingLot.zig
+++ lib/std/sync/ParkingLot.zig
@@ -13,22 +13,22 @@ pub fn ParkingLot(comptime config: anytype) type {
             return @hasDecl(@TypeOf(config), field);
         }
 
-        const Event: type = switch (hasConfig("Event")) {
+        pub const Event: type = switch (hasConfig("Event")) {
             true => config.Event,
             else => DefaultEvent,
         };
 
-        const Lock: type = switch (hasConfig("Lock")) {
+        pub const Lock: type = switch (hasConfig("Lock")) {
             true => config.Lock,
             else => DefeaultLock(Event),
         };
 
-        const Timestamp: type = switch (hasConfig("Timestamp")) {
+        pub const Timestamp: type = switch (hasConfig("Timestamp")) {
             true => config.Timestamp,
             else => DefaultTimestamp,
         };
 
-        const bucket_count: usize = switch (hasConfig("bucket_count")) {
+        pub const bucket_count: usize = switch (hasConfig("bucket_count")) {
             true => config.bucket_count,
             else => DEFAULT_BUCKET_COUNT,
         };
@@ -299,10 +299,19 @@ pub fn DefaultLock(comptime Event: type) type {
     return extern struct {
         state: usize = UNLOCKED,
 
-        const UNLOCKED: usize = 0;
-        const LOCKED: usize = 1 << 0;
-        const WAKING: usize = 1 << 1;
-        const WAITING: usize = ~(LOCKED | WAKING);
+        // True when the target is in the class of x86 chips, false otherwise.
+        // This is used to select certain cpu instructions for the occasion.
+        const is_x86 = std.builtin.arch == .i386 or .arch == .x86_64;
+        
+        // True if the target supports atomic operations of different sizes on the same address.
+        // Ive only ever observed this to be false on obscure platforms like Itanium [1], which Zig doesn't seem to target yet.
+        // [1]: (IA64 Vol 3A, Section 8.1.2.2) https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-vol-3a-part-1-manual.pdf
+        const use_byte_ops = true;
+
+        const UNLOCKED = 0;
+        const LOCKED = 1 << 0;
+        const WAKING = 1 << (if (use_byte_ops) 8 else 1);
+        const WAITING = ~@as(usize, (WAKING << 1) - 1);
 
         const Self = @This();
         const Waiter = struct {
@@ -312,13 +321,147 @@ pub fn DefaultLock(comptime Event: type) type {
             event: Event align(std.math.max(@alignOf(Event), ~WAITING + 1)),
         };
 
-        pub const Held = void;
+        pub const Held = void;        
+
+        /// Try to acquire the Lock, using the passed in state as the assume current value.
+        /// Uses Acquire memory ordering on success to see changes release()'d by last lock holder.
+        inline fn tryAcquireFast(self: *Self, state: usize) bool {
+            // On x86, "lock bts" has a smaller i-cache footprint than the alternative below.
+            if (std.builtin.arch == .i386 or .arch == .x86_64) {
+                return atomic.bitSet(
+                    &self.state,
+                    @ctz(std.math.Log2Int(usize), LOCKED),
+                    .Acquire,
+                ) == 0;
+            }
+
+            // If byte-level atomic operations are supported, try to swap only the lower byte with LOCKED.
+            // This is better than the alternative below since it doesn't contend with non-locking CAS' in other functions.
+            // It is also made possible by forcing all the non-locking state to be above the LSB byte.
+            if (use_byte_ops) {
+                return atomic.swap(
+                    @ptrCast(*u8, &self.state),
+                    LOCKED,
+                    .Acquire,
+                ) == UNLOCKED;
+            }
+
+            // For platforms that don't support any of the fancy tricks above, a normal CAS should work fine.
+            return atomic.tryCompareAndSwap(
+                &self.state,
+                state,
+                state | LOCKED,
+                .Acquire,
+                .Relaxed,
+            ) == null;
+        }
 
         pub fn acquire(self: *Self, held: *Held) void {
-            @compileError("TODO");
+            // Fast-path: speculatively try to acquire the lock assuming its unlocked.
+            if (!self.tryAcquireFast(UNLOCKED)) {
+                self.acquireSlow();
+            }
+        }
+
+        /// Slow-path: acquire the lock by possibly blocking the caller using the Event type.
+        fn acquireSlow(self: *Self) void {
+            @setCold(true);
+
+            // The waiter's Event object is lazily initialized due to possibly being expensive.
+            var waiter: Waiter = undefined;
+            var event_initialized = false;
+            defer if (event_initialized) {
+                waiter.event.deinit();
+            };
+
+            var adaptive_spin: usize = 0;
+            var state = atomic.load(&self.state, .Relaxed);
+
+            while (true) {
+                // Try to acquire the Lock if its unlocked.
+                if (state & LOCKED == 0) {
+                    if (self.tryAcquireFast(state)) {
+                        return;
+                    }
+
+                    // If we failed to acquire the lock, yield the processor a little bit.
+                    // The goal here is to reduce contention on the state, assuming theres other fast processors.
+                    if (Event.yield(adaptive_spin)) {
+                        adaptive_spin +%= 1;   
+                    } else {
+                        atomic.spinLoopHint();
+                    }
+
+                    state = atomic.load(&self.state, .Relaxed);
+                    continue;
+                }
+
+                // Spin on the lock state without thrashing its cache-line by only load()'ing.
+                // Only spin if theres no waiters (head == null) as its better to just wait instead.
+                // Also don't spin if the Event object told use that we've been spinning too long.
+                const head = @intToPtr(?*Waiter, state & WAITING);
+                if (head == null and Event.yield(adaptive_spin)) {
+                    adaptive_spin +%= 1;
+                    state = atomic.load(&self.state, .Relaxed);
+                    continue;
+                }
+
+                // Prepare the waiter to be added as the new head of the wait queue in the state.
+                // The first waiter (head == null) to be added sets its .tail to point to itself.
+                // This is further explained in the .tail lookup inside releaseSlow().
+                waiter.prev = null;
+                waiter.next = head;
+                waiter.tail = if (head == null) &waiter else null;
+                if (!event_initialized) {
+                    waiter.event.init();
+                    event_initialized = true;
+                }
+
+                // Try to enqueue the waiter onto the lock-state's wait queue.
+                // Release barrier to make the waiter writes above visible to the dequeue thread in releaseSlow().
+                if (atomic.tryCompareAndSwap(
+                    &self.state,
+                    state,
+                    (state & ~WAITING) | @ptrToInt(&waiter),
+                    .Release,
+                    .Relaxed,
+                )) |updated| {
+                    state = updated;
+                    continue;
+                }
+
+                // The waiter is now in the wait queue.
+                // Wait for a wakeup from a release() thread.
+                waiter.event.wait();
+
+                // Now that we've woken up, reset our state and try to acquire the lock again.
+                adaptive_spin = 0;
+                waiter.event.reset();
+                state = atomic.load(&self.state, .Relaxed);
+            }
         }
 
         pub fn release(self: *Self, held: *Held) void {
+            var state: usize = undefined;
+            var slow_mask: usize = undefined;
+            
+            if (use_byte_ops) {
+                atomic.store(@ptrCast(*u8, &self.state), UNLOCKED, .Release);
+                state = atomic.load(&self.state, .Relaxed);
+                slow_mask = LOCKED | WAKING;
+            } else {
+                state = atomic.fetchSub(&self.state, LOCKED, .Release);
+                slow_mask = WAKING;
+            }
+
+            if ((state & WAITING != 0) and (state & slow_mask != 0)) {
+                self.releaseSlow();
+            }
+        }
+
+        fn releaseSlow(self: *Self) void {
+            @setCold(true);
+
             @compileError("TODO");
         }
     };
